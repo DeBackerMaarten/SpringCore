ea1573b23ef3b9409014ec84223ec5270d921395
DeBackerMaarten <maarten.debacker@vdabcampus.be>
2024-01-03 13:21:18 +0100

    aanmaak class Leerling

---
+++ src/main/java/be/vdab/taken/leerlingen/LeerlingRepository.java
@@ -0,0 +1,10 @@
+package be.vdab.taken.leerlingen;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LeerlingRepository {
+    public LeerlingRepository () {
+        System.out.println("LeerlingRepository");
+    }
+}

4891b85ec545724e52b525029dc6d561458ac60d
DeBackerMaarten <maarten.debacker@vdabcampus.be>
2024-01-03 10:49:24 +0100

    aanmaak class Leerling

---
+++ src/test/java/be/vdab/taken/TakenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.taken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
